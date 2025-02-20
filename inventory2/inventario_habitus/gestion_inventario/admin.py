347b22c2e4fb318bc42eecff9be5e96a90160899
yoleider <yoleidersaa@gmail.com>
2025-02-20 09:24:56 -0500

    Delete inventory2/inventario_habitus directory

--- inventory2/inventario_habitus/gestion_inventario/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Usuario, Almacen, Item, SolicitudTraslado
-
-admin.site.register(Usuario)
-admin.site.register(Almacen)
-admin.site.register(Item)
-admin.site.register(SolicitudTraslado)
